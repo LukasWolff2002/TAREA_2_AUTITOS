24b82dcb2c16b6226c18075bbcd65164c916b5ab
felipe vicencio <favicencio@miuandes.cl>
2024-08-31 16:24:50 -0400

    agrego 2012

--- CODIGO/2.1 y 2.3/promedio_viajes.py
+++ CODIGO/2.1 y 2.3/promedio_viajes.py
@@ -8,15 +8,15 @@ def promedio_hogares(df):
     hogares = df['numero_hogares']    
     return(y, hogares)
 
-# Uso de la función
-# ruta_archivo = 'BASES_DE_DATOS/ESI2012-PERSONAS.csv'
-# df = coef_promediados(ruta_archivo)
+#Uso de la función
+ruta_archivo = 'BASES_DE_DATOS/ESI2012-PERSONAS.csv'
+df = coef_promediados(ruta_archivo)
 
-# numero_viajes, cantidad_hogares = promedio_hogares(df)
-# print('2012')
-# print(f"El promedio de viajes por hogar es de {numero_viajes:.2f} y se analizaron {cantidad_hogares} hogares.")
+numero_viajes, cantidad_hogares = promedio_hogares(df)
+print('2012')
+print(f"El promedio de viajes por hogar es de {numero_viajes:.2f} y se analizaron {cantidad_hogares} hogares.")
 
-# print('')
+print('')
 ruta_archivo = 'BASES_DE_DATOS/ESI2017-PERSONAS.csv'
 df = coef_promediados(ruta_archivo)
 
